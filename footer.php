cbca713bd253aa244fc2d073498ea7f9d6a03eb2
Hayden <hello@haydentomas.com>
2019-07-13 22:36:23 +0100

    Prefooter css

--- footer.php
+++ footer.php
@@ -14,19 +14,27 @@
 	</div><!-- #content -->
 <div class="prefooter">
 	<div class="prelogo">logo</div>
-	<div class="pretext"><h2 class="h1">
-	If you want to take your website or application to the next level, we’ve got the experience to get you there. 
-	</h2></div>
+	<div class="pretext">
+		<h2 class="h1">
+			If you want to take your website or application to the next level, we’ve got the experience to get you
+			there.
+		</h2>
+		<p>
+			Since 2006, we've helped small businesses, Fortune 500 companies, and entrepreneurs build better products
+			with forward-thinking design, data-driven insight, and improved user experiences.
+		</p>
+	</div>
 </div>
-	<footer>
+<footer>
 	<div class="footitem">
-	<h3>Pixaful &copy; 2019</h3>
-	<p>90 Yerbury Road,<br/>Islington,London,<br/>N19 4RS</p>
-	<p><a href="#">Privacy Policy</a> - <a href="#">Terms</a></p>
+		<h3>Pixaful &copy; 2019</h3>
+		<p>90 Yerbury Road,<br />Islington,London,<br />N19 4RS</p>
+		<p><a href="#">Privacy Policy</a> - <a href="#">Terms</a></p>
 	</div>
 	<div class="footitem">
 		<h3>Join Our Community</h3>
-		<p>We'll help you become a better creative with articles, resources, & behind-the-scenes content delivered straight to your inbox. </p>
+		<p>We'll help you become a better creative with articles, resources, & behind-the-scenes content delivered
+			straight to your inbox. </p>
 		<p><a href="#">Sign up today</a></p>
 	</div>
 	<div class="footitem">
@@ -37,14 +45,14 @@
 		</ul>
 	</div>
 	<div class="footitem">
-	<h3>Contact Us</h3>
-	<ul>
+		<h3>Contact Us</h3>
+		<ul>
 			<li><a href="#">hello@pixaful.com</a></li>
 			<li><a href="#">07803 501 235</a></li>
 		</ul>
 	</div>
-	</footer>
-	
+</footer>
+
 </div>
 
 <?php wp_footer(); ?>
