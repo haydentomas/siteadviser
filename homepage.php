20e79b9940253a6c479af3f7561fb66202b54e12
Hayden <hello@haydentomas.com>
2019-07-14 23:22:40 +0100

    Testimonials with acf pro

--- homepage.php
+++ homepage.php
@@ -1,10 +1,30 @@
 <?php /* Template Name: Home */ 
 get_header();
 ?>
+<div class="testimonial">
+<?php
+  $args = array(
+    'post_type'=>'testimonials', 
+    'orderby'=>'rand', 
+    'posts_per_page'=>'1'
+  );
+
+  $testimonials=new WP_Query($args);
+
+  while ($testimonials->have_posts()) : $testimonials->the_post(); 
+?>
+    <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+	<?php the_excerpt(); // or the_content(); ?>
+	<?php the_post_thumbnail(); ?>
+	<?php the_field( 'name' ); ?>
+	<a href="<?php the_field( 'website_address' ); ?>" target="_blank"><?php the_field( 'url_display' ); ?></a>
+
+<?php 
+  endwhile;
+  wp_reset_postdata();
+?>
+</div>
 <div class="postgrid">
-
-
-
 		<?php // Display blog posts on any page @ https://m0n.co/l
 		$temp = $wp_query; $wp_query= null;
 		$wp_query = new WP_Query(); $wp_query->query('posts_per_page=3' . '&paged='.$paged);
